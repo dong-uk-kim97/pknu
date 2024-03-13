f9ba3ea045c3c62d2d5fbca4546d7f51c69a4340
DongUk Kim <kdw24739577@gmail.com>
2024-03-13 17:23:07 +0900

    Create algorithm.py

---
+++ 20240313/algorithm.py
@@ -0,0 +1,41 @@
+a = [7,2,1,8,9,10]
+# max함수 쓰지 않고 max값 구하기
+max = 0
+for i in a:
+    if i>max:
+        max=i
+    else:continue
+print(max)
+
+# 최댓값과 그 전 값 구하기
+for i in range(len(a)-1):
+    if a[i]>a[i+1]:
+        a[i+1]=a[i]
+    else:
+        continue
+print(a[4],a[5])
+
+for i in range(len(a)-1):
+    if a[i]>a[i+1]:
+        a[i],a[i+1] = a[i+1],a[i]
+    else:
+        continue
+print(a[4],a[5])
+
+# 알파벳 순서대로 만들기
+a = ['a','a','c','b','D','E']
+
+for i in range(len(a)):
+    if a[i]=='a':
+        a[0]='a'
+    elif a[i]=='b':
+        a[1]='b'
+    elif a[i]=='c':
+        a[2]='c'
+    elif a[i]=='D':
+        a[3]='D'
+    elif a[i]=='E':
+        a[4]='E'
+a.pop(5)
+print(a)
+
